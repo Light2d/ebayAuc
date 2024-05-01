6b4aafc36ff44afd902d909cc02da343be9b6b94
Light2d <lighttt2d@gmail.com>
2024-05-01 15:47:38 +0300

    изменил страницу оплаты и продукт

--- static/scripts/payment.js
+++ static/scripts/payment.js
@@ -4,8 +4,8 @@ document.addEventListener("DOMContentLoaded", function() {
         // Проверяем, все ли поля заполнены
         var inputs = document.querySelectorAll('.form__input');
         var allFieldsFilled = true;
-        var countrySelect = document.querySelector('.form__counrty');
-        var country = countrySelect.options[countrySelect.selectedIndex].value;
+        var countrySelect = document.querySelector('.selected-item');
+        var country = countrySelect.textContent.trim();
 
         if (country === '') {
             alert('Please select a country.');
@@ -21,12 +21,11 @@ document.addEventListener("DOMContentLoaded", function() {
             // Копируем значения из полей ввода
             var firstName = document.querySelector('.form__first').value;
             var lastName = document.querySelector('.form__last').value;
-            var street = document.querySelector('.form__steer').value;
+            var street = document.querySelector('.form__street').value;
             var street2 = document.querySelector('.form__street2').value;
             var city = document.querySelector('.form__city').value;
             var state = document.querySelector('.form__state').value;
             var zip = document.querySelector('.form__zip').value;
-            var country = document.querySelector('.form__counrty').value;
             var email = document.querySelector('.form__email').value;
             var phone = document.querySelector('.form__phone').value;
 
@@ -63,12 +62,12 @@ document.addEventListener("DOMContentLoaded", function() {
         // Копируем данные из элементов формы form__infoTwo в инпуты формы form__info
         document.querySelector('.form__first').value = document.querySelector('.data__first').innerText;
         document.querySelector('.form__last').value = document.querySelector('.data__last').innerText;
-        document.querySelector('.form__steer').value = document.querySelector('.data__street').innerText;
+        document.querySelector('.form__street').value = document.querySelector('.data__street').innerText;
         document.querySelector('.form__street2').value = document.querySelector('.data__street2').innerText;
         document.querySelector('.form__city').value = document.querySelector('.data__city').innerText;
         document.querySelector('.form__state').value = document.querySelector('.data__state').innerText;
         document.querySelector('.form__zip').value = document.querySelector('.data__zip').innerText;
-        document.querySelector('.form__counrty').value = document.querySelector('.data__country').innerText;
+        document.querySelector('.form__country').value = document.querySelector('.selected-item').textContent.trim();
         document.querySelector('.form__email').value = document.querySelector('.data__email').innerText;
         document.querySelector('.form__phone').value = document.querySelector('.data__phone').innerText;
     });
@@ -85,13 +84,16 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
+var orderBtn = document.getElementById("orderBtn");
 
+orderBtn.addEventListener("click", function() {
+    window.onbeforeunload = null;
 
+});
 window.onbeforeunload = function() {
     return "Data will be lost if you leave the page, are you sure?";
-  };
-
-  function formatPhoneNumber(input) {
+};
+function formatPhoneNumber(input) {
     let phoneNumber = input.value.replace(/\D/g, '');
 
     phoneNumber = phoneNumber.replace(/(\d{1})(\d{3})(\d{3})(\d{4})/, '+$1 ($2) $3-$4');
